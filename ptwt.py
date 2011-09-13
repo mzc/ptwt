aac393646300ed154e660d8016ee63a581ccbc62
Max Z. Chao <max.z.chao@gmail.com>
2011-09-13 19:30:08 +1000

    Persist access token

--- ptwt.py
+++ ptwt.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 
 import sys
+import os
+import errno
 import string
 import getopt
 import subprocess
+try:
+    import simplejson as json
+except ImportError:
+    import json
 
-from secret import *
 from restapi import OAuthOOB, OAuthConn
 from restapi import Client
 
 REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
 AUTHENTICATE_URL  = "https://api.twitter.com/oauth/authenticate"
 ACCESS_TOKEN_URL  = "https://api.twitter.com/oauth/access_token"
+CONSUMER_KEY      = 'dB2Uh0OXgDtAxp6KHK9Q'
+CONSUMER_SECRET   = 'lKcsEGOtdNolro2aWBG1zwomhftIdKiimxPTaT3nSRo'
 
 commands = {
     'help': lambda oauth_conn, twitter, args: usage(),
@@ -29,31 +36,31 @@ commands = {
 def usage():
     print 'usage: TODO'
 
-def check_json_error(json):
+def check_entity_error(entity):
     try:
-        if json.has_key('errors'):
-            return json['errors'][0]['message']
-        elif json.has_key('error'):
-            return json['error']
+        if entity.has_key('errors'):
+            return entity['errors'][0]['message']
+        elif entity.has_key('error'):
+            return entity['error']
         else:
             return False
     except:
         return False
 
-def prompt_error(json):
-    error = check_json_error(json)
+def prompt_error(entity):
+    error = check_entity_error(entity)
     if error:
         print error
         return True
     else:
         return False
 
-def print_timeline(jsons, verbose):
-    for json in jsons:
-        screen_name = json['user']['screen_name']
-        name = json['user']['name']
-        text = json['text']
-        created_at = json['created_at']
+def print_timeline(entities, verbose):
+    for entity in entities:
+        screen_name = entity['user']['screen_name']
+        name = entity['user']['name']
+        text = entity['text']
+        created_at = entity['created_at']
         
         if verbose:
             print '%s: %s at \'%s\'' % (screen_name, name, created_at)
@@ -78,9 +85,9 @@ def home_timeline(oauth_conn, twitter, args):
         else:
             assert False, 'unexcepted option'
 
-    jsons = statuses.home_timeline(oauth_conn)
-    if not prompt_error(jsons):
-        print_timeline(jsons, verbose)
+    entities = statuses.home_timeline(oauth_conn)
+    if not prompt_error(entities):
+        print_timeline(entities, verbose)
 
 def user_timeline(oauth_conn, twitter, args):
     statuses = twitter.Statuses()
@@ -100,12 +107,12 @@ def user_timeline(oauth_conn, twitter, args):
 
     if args:
         screen_name = args[0]
-        jsons = statuses.user_timeline(oauth_conn, screen_name = screen_name)
+        entities = statuses.user_timeline(oauth_conn, screen_name = screen_name)
     else:
-        jsons = statuses.user_timeline(oauth_conn)
+        entities = statuses.user_timeline(oauth_conn)
 
-    if not prompt_error(jsons):
-        print_timeline(jsons, verbose)
+    if not prompt_error(entities):
+        print_timeline(entities, verbose)
         
 def public_timeline(oauth_conn, twitter, args):
     statuses = twitter.Statuses()
@@ -123,14 +130,14 @@ def public_timeline(oauth_conn, twitter, args):
         else:
             assert False, 'unexcepted option'
 
-    jsons = statuses.public_timeline(oauth_conn)
-    if not prompt_error(jsons):
-        print_timeline(jsons, verbose)
+    entities = statuses.public_timeline(oauth_conn)
+    if not prompt_error(entities):
+        print_timeline(entities, verbose)
 
-def print_lists(jsons, verbose):
-    for json in jsons:
-        slug = json['slug']
-        description = json['description']
+def print_lists(entities, verbose):
+    for entity in entities:
+        slug = entity['slug']
+        description = entity['description']
         if verbose:
             print '%s: %s' % (slug, description)
         else:
@@ -155,29 +162,29 @@ def lists(oauth_conn, twitter, args):
     if len(args) >= 2:
         # get the timeline of the specified list
         owner_screen_name, slug = args[0], args[1]
-        jsons = lists.statuses(oauth_conn, owner_screen_name = owner_screen_name, slug = slug)
-        if not prompt_error(jsons):
-            print_timeline(jsons, verbose)
+        entities = lists.statuses(oauth_conn, owner_screen_name = owner_screen_name, slug = slug)
+        if not prompt_error(entities):
+            print_timeline(entities, verbose)
     else:
         # get all the lists of a specified user
         if args:
             screen_name = args[0]
-            jsons = lists.all(oauth_conn, screen_name = screen_name)
+            entities = lists.all(oauth_conn, screen_name = screen_name)
         else:
-            jsons = lists.all(oauth_conn)
+            entities = lists.all(oauth_conn)
 
-        if not prompt_error(jsons):
-            print_lists(jsons, verbose)
+        if not prompt_error(entities):
+            print_lists(entities, verbose)
     
-def print_users(jsons, verbose):
-    for json in jsons:
-        screen_name = json['screen_name']
-        name = json['name']
-        id_str = json['id_str']
-        location = json['location']
-        created_at = json['created_at']
-        description = json['description']
-        time_zone = json['time_zone']
+def print_users(entities, verbose):
+    for entity in entities:
+        screen_name = entity['screen_name']
+        name = entity['name']
+        id_str = entity['id_str']
+        location = entity['location']
+        created_at = entity['created_at']
+        description = entity['description']
+        time_zone = entity['time_zone']
         if verbose:
             print '%s: %s at %s (%s)' % (screen_name, name, location, time_zone)
             print '\tID: %s' % id_str
@@ -188,13 +195,13 @@ def print_users(jsons, verbose):
     
 def users_lookup_by_screen_name(oauth_conn, users, args):
     screen_names = string.join(args, ',')
-    jsons = users.lookup(oauth_conn, screen_name = screen_names)
-    return jsons
+    entities = users.lookup(oauth_conn, screen_name = screen_names)
+    return entities
 
 def users_lookup_by_user_id(oauth_conn, users, args):
     user_id = string.join(args[0:100], ',') # TODO: improve it
-    jsons = users.lookup(oauth_conn, user_id = user_id)
-    return jsons
+    entities = users.lookup(oauth_conn, user_id = user_id)
+    return entities
 
 def users_lookup(oauth_conn, twitter, args):
     users = twitter.Users()
@@ -215,9 +222,9 @@ def users_lookup(oauth_conn, twitter, args):
         usage()
         return
 
-    jsons = users_lookup_by_screen_name(oauth_conn, users, args)
-    if not prompt_error(jsons):
-        print_users(jsons, verbose)
+    entities = users_lookup_by_screen_name(oauth_conn, users, args)
+    if not prompt_error(entities):
+        print_users(entities, verbose)
 
 def friends(oauth_conn, twitter, args):
     friends = twitter.Friends()
@@ -247,9 +254,9 @@ def friends(oauth_conn, twitter, args):
     users_string = map(str, users_int)
     
     users = twitter.Users()
-    jsons = users_lookup_by_user_id(oauth_conn, users, users_string)
-    if not prompt_error(jsons):
-        print_users(jsons, verbose)
+    entities = users_lookup_by_user_id(oauth_conn, users, users_string)
+    if not prompt_error(entities):
+        print_users(entities, verbose)
 
 def followers(oauth_conn, twitter, args):
     followers = twitter.Followers()
@@ -279,9 +286,9 @@ def followers(oauth_conn, twitter, args):
     users_string = map(str, users_int)
     
     users = twitter.Users()
-    jsons = users_lookup_by_user_id(oauth_conn, users, users_string)
-    if not prompt_error(jsons):
-        print_users(jsons, verbose)
+    entities = users_lookup_by_user_id(oauth_conn, users, users_string)
+    if not prompt_error(entities):
+        print_users(entities, verbose)
 
 def interactive(oauth_conn, twitter):
     while True:
@@ -309,22 +316,56 @@ def one_shot(oauth_conn, twitter):
     except KeyError:
         print 'Unknow command:', cmd
 
-def authorize():
+def authorize_oob():
     oauth_oob = OAuthOOB(REQUEST_TOKEN_URL, AUTHENTICATE_URL, ACCESS_TOKEN_URL)
     oauth_conn = OAuthConn(CONSUMER_KEY, CONSUMER_SECRET)
     oauth_conn = oauth_oob.get_temp_credentials(oauth_conn)
     
     subprocess.call(['chromium', oauth_oob.temp_credentials_url])
-    sys.stdout.write('PIN:')
+    sys.stdout.write('PIN: ')
     pin_code = raw_input()
     
     oauth_conn = oauth_oob.get_credentials(oauth_conn, pin_code)
-    
+    return oauth_conn
+
+def authorize(settings):
+    try:
+        config = json.load(open(settings))
+        
+        consumer_key    = config['consumer_key']
+        consumer_secret = config['consumer_secret']
+        token_key       = config['token_key']
+        token_secret    = config['token_secret']
+        
+        oauth_conn = OAuthConn(consumer_key, consumer_secret, token_key, token_secret)
+    except Exception:
+        oauth_conn = authorize_oob()
+        consumer_key    = CONSUMER_KEY
+        consumer_secret = CONSUMER_SECRET
+        token_key       = oauth_conn.token_key
+        token_secret    = oauth_conn.token_secret
+
+        f = open(settings, 'w')
+        f. write(json.dumps({ 'consumer_key'    : consumer_key,
+                              'consumer_secret' : consumer_secret,
+                              'token_key'       : token_key,
+                              'token_secret'    : token_secret, }, indent=4))
     return oauth_conn
         
 def main():
+    settings_dir = os.getenv('HOME') + '/.config/ptwt/' 
+    settings =  settings_dir + 'settings'
+    try:
+        os.stat(settings_dir)
+    except OSError, e:
+        if e.errno == errno.ENOENT:
+            os.makedirs(settings_dir)
+        else:
+            sys.stderr.write('Failed to create config dir: ' + settings_dir + '\n')
+            exit(1)
+    
     twitter = Client('twitter')
-    oauth_conn = authorize()
+    oauth_conn = authorize(settings)
 
     if len(sys.argv) == 1:
         interactive(oauth_conn, twitter)
